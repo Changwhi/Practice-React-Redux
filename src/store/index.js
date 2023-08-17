0480171dba98cc4d597a0a1a76ad1fde694a418b
Changwhi <whinada@gmail.com>
2023-08-16 21:21:22 -0700

    feat: implement main page and store

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import {createStore} from 'redux';
+
+const countReducer = (state = { counter: 0 }, action) => {
+   if (state.type === 'increment') {
+        return {
+        counter: state.counter + 1
+    };
+    }
+
+    if (state.type === 'decrement') {
+        return {
+        counter: state.counter - 1
+        };
+    }
+};
+
+const store = createStore(countReducer);
+
+export default store;
+
